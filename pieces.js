65e27230dc71374b75c80feb557754d3e035fe30
Ben Abdallah Houda <ahhouda@yahoo.fr>
2023-02-02 19:09:45 +0100

    mettre à jour l'affichage

--- pieces.js
+++ pieces.js
@@ -1,36 +1,37 @@
-//recuper les pieces depuis le ficher json//
-const reponse = await fetch ('pieces-autos.json');
+// Récupération des pièces depuis le fichier JSON
+const reponse = await fetch('pieces-autos.json');
 const pieces = await reponse.json();
-//balise//
 
-for (let i in pieces){
+for (let i = 0; i < pieces.length; i++) {
+
     const article = pieces[i];
-
+    // Récupération de l'élément du DOM qui accueillera les fiches
+    const sectionFiches = document.querySelector(".fiches");
+    // Création d’une balise dédiée à une pièce automobile
+    const pieceElement = document.createElement("article");
+    // Création des balises 
     const imageElement = document.createElement("img");
     imageElement.src = article.image;
-
     const nomElement = document.createElement("h2");
     nomElement.innerText = article.nom;
-
     const prixElement = document.createElement("p");
-    // ? indique que si le prix inferieur de 35 affiche € si superieur €€€
     prixElement.innerText = `Prix: ${article.prix} € (${article.prix < 35 ? "€" : "€€€"})`;
-
     const categorieElement = document.createElement("p");
-    // on utilise ?? si un varible n'existe pas
     categorieElement.innerText = article.categorie ?? "(aucune catégorie)";
-
     const descriptionElement = document.createElement("p");
-    descriptionElement.innerText = article.description;
+    descriptionElement.innerText = article.description ?? "Pas de description pour le moment.";
+    const stockElement = document.createElement("p");
+    stockElement.innerText = article.disponibilite ? "En stock" : "Rupture de stock";
+    
+    // On rattache la balise article a la section Fiches
+    sectionFiches.appendChild(pieceElement);
+    // On rattache l’image à pieceElement (la balise article)
+    pieceElement.appendChild(imageElement);
+    pieceElement.appendChild(nomElement);
+    pieceElement.appendChild(prixElement);
+    pieceElement.appendChild(categorieElement);
+    //Ajout des éléments au DOM pour l'exercice
+    pieceElement.appendChild(descriptionElement);
+    pieceElement.appendChild(stockElement);
 
-    const disponibiliteElement = document.createElement("p");
-    disponibiliteElement.innerText = article.disponibilite ? "En stock" : "Rupture de stock" ;
-    //rattachement balise html parent a DOM
-    const sectionFiches = document.querySelector(".fiches");
-    sectionFiches.appendChild(imageElement);
-    sectionFiches.appendChild(nomElement);
-    sectionFiches.appendChild(prixElement);
-    sectionFiches.appendChild(categorieElement);
-    sectionFiches.appendChild(descriptionElement);
-    sectionFiches.appendChild(disponibiliteElement);
-}
+ }
